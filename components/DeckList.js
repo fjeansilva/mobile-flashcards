5cb44f789d8bbe146147e901daf4eb1533721433
Jean Silva <jeancarlos.silva@me.com>
2018-08-03 02:47:18 -0300

    refactor

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   FlatList,
-// } from 'react-native';
-// import DeckListItem from './DeckListItem';
-
-// const handleItem = ({ item }) => <DeckListItem key={item.key} {...item} />;
-
-// const DeckList = ({ data }) => (
-//   <FlatList
-//     data={data}
-//     renderItem={handleItem}
-//     style={styles.list}
-//   />
-// );
-
-// const styles = StyleSheet.create({
-//   list: {
-//     flex: 1,
-//     borderWidth: 2,
-//     borderColor: 'red',
-//     paddingLeft: 10,
-//     paddingRight: 10,
-//   }
-// });
-
-// export default DeckList;
-
 
 import React, {Component} from 'react'
 import {View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native'
